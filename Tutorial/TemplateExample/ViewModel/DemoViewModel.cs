f5cf772a61d165c150021de9457b29c2012ea9d1
LAPTOP-9V17EEST\Mark <yishsunsuncode@gmail.com>
2022-07-21 20:53:00 +0800

    Template教學

---
+++ Tutorial/TemplateExample/ViewModel/DemoViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Tutorial.TemplateExample.ViewModel
+{
+    class DemoViewModel
+    {
+        public ICommand Command { get; }
+        public DemoViewModel() 
+        {
+            Command = new RelayCommand(() =>
+            {
+                MessageBox.Show("Click");
+            });
+        }
+    }
+}
---
+++ Tutorial/TemplateExample/Xaml/AttachedProperty.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Tutorial.TemplateExample.Xaml
+{
+    static class AttachedProperty
+    {
+
+
+
+        public static int GetCheckBoxSize(DependencyObject obj)
+        {
+            return (int)obj.GetValue(CheckBoxSizeProperty);
+        }
+
+        public static void SetCheckBoxSize(DependencyObject obj, int value)
+        {
+            obj.SetValue(CheckBoxSizeProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for CheckBoxSize.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CheckBoxSizeProperty =
+            DependencyProperty.RegisterAttached("CheckBoxSize", typeof(int), typeof(AttachedProperty), new PropertyMetadata(100));
+
+
+
+    }
+}
